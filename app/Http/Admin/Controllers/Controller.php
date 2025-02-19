47c5a47a1f3e30b4b3571d042f58f27735e1c71c
ZYB <285038160@qq.com>
2025-02-19 15:30:07 +0800

    提交

---
+++ app/Http/Admin/Controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Admin\Controllers;
+
+abstract class Controller
+{
+    //
+}
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+
+
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <AdminLayout>
+        <h1>
+            Welcome Think
+        </h1>
+    </AdminLayout>
+</template>
